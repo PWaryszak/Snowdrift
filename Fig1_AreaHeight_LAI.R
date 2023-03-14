8f506cda59109843074c836e8fc8519315347a12
Pawel Waryszak <pawel.waryszak.com>
2023-03-14 16:20:44 +1000

    making that figure look even prettier

--- Fig1_AreaHeight_LAI.R
+++ Fig1_AreaHeight_LAI.R
@@ -18,9 +18,10 @@ summary(snow2$height_cm)#Summary of focus shrub plant height_cm:
 # 15.00   30.00   45.00   46.57   60.00  110.00       4 
 sd(snow2$height_cm, na.rm = TRUE)#19.23388
 
+
 #HEIGHT by FOCUS SPECIES:
 Six_height_cm <- snow2 %>%
-  group_by(shrub, genus_species, shrub_genus, shrub_species) %>%
+  group_by(Region, shrub, genus_species, shrub_genus, shrub_species) %>%
   summarize( mean_height_cm = round (mean (height_cm, na.rm=T),1),
              n_height_cm = length(height_cm),
              sd_height_cm = round (sd(height_cm, na.rm=T),1),
@@ -41,7 +42,7 @@ Six_height_cm <- snow2 %>%
 
 
 Six_height_cm
-#write.table(Six_height_cm, file = "TRAITsumstats.txt", sep = ",", quote = FALSE, row.names = F)  #Save as table.txt and then copy into Word, 
+#write.table(Six_height_cm, file = "6TargetSgrubsTraits.txt", sep = ",", quote = FALSE, row.names = F)  #Save as table.txt and then copy into Word, 
 #in Word to get a Word-Table, select it all, go to Table → Convert → Convert Text to Tabl3
 
 #Turning Table 1 (Six_height_cm) into Plot===========
@@ -50,12 +51,13 @@ names(Six_height_cm)
 #Panel Plot of Table 1:
 s1 <- ggplot(Six_height_cm, aes( x =  "", y=mean_height_cm)) +
   geom_point(aes(size = as.numeric(mean_area_cm3), fill=mean_LAI),shape=21, colour = "black")+
-  geom_errorbar( aes(ymin= mean_height_cm+se_height_cm,
-                     ymax = mean_height_cm-se_height_cm), width=.2, colour = "white")+
+  
+  #errror bars look like little H letter. We descibe the in captions instead:
+  ##geom_errorbar( aes(ymin= mean_height_cm+se_height_cm, ymax = mean_height_cm-se_height_cm), width=.2, colour = "white")+
   scale_size(range = c(10,20))+
   scale_y_continuous(limits = c(1, 80),expand = c(0, 0))+
   scale_fill_gradient(low = "grey", high = "black")+
-  facet_grid(.~shrub_genus + shrub_species) +
+  facet_grid(.~Region+shrub_genus + shrub_species) +
   
   
   theme_classic()+
@@ -68,7 +70,7 @@ s1 <- ggplot(Six_height_cm, aes( x =  "", y=mean_height_cm)) +
         axis.ticks.x.bottom = element_blank(),
         plot.title = element_text(size=20, lineheight=1.8, face="bold", hjust = 0.5),
         legend.title = element_text(size=16, face="bold"),
-        legend.text = element_text(size = 8),
+        legend.text = element_text(size = 10),
         #legend.key = element_rect( fill = "white", color = "black"),
         legend.key=element_blank(), #Removes the frames around the points in legends
         legend.box.background = element_rect(),
@@ -87,5 +89,57 @@ s1 <- ggplot(Six_height_cm, aes( x =  "", y=mean_height_cm)) +
         legend.position = c(0.8 , 0.25)) 
 
 s1
-#ggsave(s1, dpi=300, width = 2401, height = 2037, units = "px", filename = "Table1_PlotNewUpdated1.jpg")
+ggsave(s1, dpi=300, width = 2401, height = 2037, units = "px", filename = "Table1_PlotNewUpdated3_Regions2.jpg")
 
+
+
+#Region as shape:
+#Panel Plot of Table 1:
+#Relevel to clup BHP/KNP species together
+levels(as.factor(Six_height_cm$shrub_genus))
+
+
+s1 <- ggplot(Six_height_cm, aes( x =  "", y=mean_height_cm)) +
+  geom_point(aes(size = as.numeric(mean_area_cm3), fill=mean_LAI,shape=Region), colour = "black")+
+  scale_shape_manual(values=c(21,22))+
+  
+  #errror bars look like little H letter. We descibe the in captions instead:
+  ##geom_errorbar( aes(ymin= mean_height_cm+se_height_cm, ymax = mean_height_cm-se_height_cm), width=.2, colour = "white")+
+  scale_size(range = c(10,20))+
+  scale_y_continuous(limits = c(1, 80),expand = c(0, 0))+
+  scale_fill_gradient(low = "grey", high = "black")+
+  facet_grid(.~Region+shrub_genus + shrub_species) +
+  
+  
+  theme_classic()+
+  labs( x = "Target shrub", y = "Mean target shrub height (cm)",
+         fill = "LAI:", shape = "Region: ",size = "Area:", )+
+  
+  guides(shape = guide_legend(override.aes = list(size = 5)))+  # Adjust the symbol size in legends
+
+  theme(axis.text.x = element_blank(),   #text(size=16,hjust=.5,angle=45,vjust=1,face="italic", color="black"),
+        axis.text.y = element_text(size=16, hjust=.5,vjust=0,face="plain", color="black"),  
+        axis.title.x = element_blank(),    #text(size=22,hjust=0.5,vjust=9,face="plain"),
+        axis.title.y = element_text(size=22),
+        axis.ticks.x.bottom = element_blank(),
+        plot.title = element_text(size=20, lineheight=1.8, face="bold", hjust = 0.5),
+        legend.title = element_text(size=16, face="bold"),
+        legend.text = element_text(size = 10),
+        #legend.key = element_rect( fill = "white", color = "black"),
+        legend.key=element_blank(), #Removes the frames around the points in legends
+        legend.box.background = element_rect(),
+        legend.box.margin = margin(6, 6, 6, 6),
+        legend.direction="vertical",
+        
+        strip.text=element_text(size=14, face = "italic"),
+        strip.background = element_rect(colour="white"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        
+        axis.line.x.bottom=element_blank(),
+        
+        legend.box = "horizontal",
+        legend.position = c(0.8 , 0.25)) 
+
+s1
