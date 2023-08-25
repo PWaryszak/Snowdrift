e3ae149995acaabc348c08095a512790c526bcd4
Pawel Waryszak <pawel.waryszak.com>
2023-08-25 19:49:56 +1000

    Extra Plot with split for two burial dephts.

--- ShrubLitter_DATA.R
+++ ShrubLitter_DATA.R
@@ -1,4 +1,4 @@
-#ShrubLitter_DATA
+#DATA===============
 library("readxl")
 library(tidyverse)
 library(lme4)
@@ -11,26 +11,59 @@ library(ggpmisc)
 
 
 ShrubLitter_DATA <- read_xlsx("ShrubLitter_DATA.xlsx", sheet = "ShrubLitter_DATA")
-head(ShrubLitter_DATA); summary(ShrubLitter_DATA); names(ShrubLitter_DATA)
-table(ShrubLitter_DATA$site)
-#  B   H  KB   S 
-#174 176 173 110 
+head(ShrubLitter_DATA); summary(ShrubLitter_DATA); names(ShrubLitter_DATA);table(ShrubLitter_DATA$site)
 
-#Run Regression on surfaceGDDs:
-soilgdds_model <- lmer(massloss_gday ~ surfaceGDDs + (1|site), data = ShrubLitter_DATA) 
-summary(lm(massloss_gday ~ surfaceGDDs, data = ShrubLitter_DATA)) 
-plot(lm(massloss_gday ~ surfaceGDDs, data = ShrubLitter_DATA)) 
-
-#Table:
-tab_model(soilgdds_model, show.stat=T, show.se=T, show.ci = F , digits = 7)
-
-ShrubLitter_DATA $ months <- as.factor(ShrubLitter_DATA $ Months_Of_Deployment)
+ShrubLitter_DATA $ months <- as.factor(ShrubLitter_DATA $ Months_Of_Deployment) #turn numbers into factor for plotting
 unique(ShrubLitter_DATA$months)
 
-#PLOT:
+
+#Table:==========
+#run Lmer:
+soilgdds_model <- lmer(massloss_gday ~ surfaceGDDs + (1|site), data = ShrubLitter_DATA) 
+tab_model(soilgdds_model, show.stat=T, show.se=T, show.ci = F , digits = 7)
+
+
+#Run lm surfaceGDDs:
+lm_gdd <- lm(massloss_mgday ~ surfaceGDDs, data = ShrubLitter_DATA)
+plot(lm_gdd) 
+summary(lm_gdd)
+tab_model(lm_gdd, show.stat=T, show.se=T, show.ci = F , digits = 3)
+
+#PLOT:==========
 names(ShrubLitter_DATA)
+ShrubLitter_DATA$B <-  as.factor(ShrubLitter_DATA$burialdepth_cm)
+
+, shape = B
+
+ggplot(aes(y = massloss_mgday, x = surfaceGDDs, shape= B), data = ShrubLitter_DATA) +
+  geom_point(size = 4,aes(fill=months), stroke = 0.5) +
+  labs( title = "",
+    y = "Mass loss (mg/day)",
+    x = "Growing degree days",
+    shape = "Deployment depth: ",
+    color = "Months of deployment:") +       #,  shape = "Site" or burialdepth_cm
+  
+  scale_shape_manual(values = c(19,15))+
+  scale_color_manual(values = c("white", "grey70", "#666666")) +
+  #scale_fill_manual(values = c("white", "grey80", "#666666")) +
+  facet_grid(. ~ species) +
+  theme_bw() +
+    guides(colour = guide_legend(show = FALSE)) +
+
+    geom_smooth( method = "lm", se = TRUE, size = 1, color = "black") +
+  theme(legend.position = "top",
+    legend.key.size = unit(1.5, "lines"),
+    legend.background = element_rect(fill = "white", color = "black"),
+    axis.title = element_text(size = 18),
+    panel.grid = element_blank(),
+    strip.text = element_text(face = "italic", size = 14),
+    axis.text = element_text(size = 12, color = "black"))
 
 
+#ggsave(width = 12, height = 7, file = "MassLoss_Species_Lm.png")
+
+
+#No split by Burial Depth Plot:
 ggplot(aes(y = massloss_mgday, x = surfaceGDDs, color = months), data = ShrubLitter_DATA) +
   geom_point(size = 4, aes(fill = months), shape = 21, stroke = 0.5, color = "black") +
   labs( title = "",
@@ -53,5 +86,4 @@ ggplot(aes(y = massloss_mgday, x = surfaceGDDs, color = months), data = ShrubLit
     strip.text = element_text(face = "italic", size = 14),
     axis.text = element_text(size = 12, color = "black"))
 
-ggsave(width = 12, height = 7, file = "MassLoss_Species_Lm.png")
-
+#ggsave(width = 12, height = 7, file = "MassLoss_Species_Lm.png")
