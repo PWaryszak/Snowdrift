4143c685356acad8aa84e414aaf81fe614f1e01a
Pawel Waryszak <pawel.waryszak.com>
2023-10-05 12:59:37 +1000

    changing to ash line the lm of 15cm burial depth + produced stats table with separate R2 values and p values for each regression lines in the figure.

--- ShrubLitter_DATA.R
+++ ShrubLitter_DATA.R
@@ -16,74 +16,78 @@ head(ShrubLitter_DATA); summary(ShrubLitter_DATA); names(ShrubLitter_DATA);table
 ShrubLitter_DATA $ months <- as.factor(ShrubLitter_DATA $ Months_Of_Deployment) #turn numbers into factor for plotting
 unique(ShrubLitter_DATA$months)
 
+names(ShrubLitter_DATA)
+ShrubLitter_DATA$Burial_cm <-  as.factor(ShrubLitter_DATA$burialdepth_cm)
 
-#Table:==========
+#Table Combined:==========
 #run Lmer:
 soilgdds_model <- lmer(massloss_gday ~ surfaceGDDs + (1|site), data = ShrubLitter_DATA) 
 tab_model(soilgdds_model, show.stat=T, show.se=T, show.ci = F , digits = 7)
 
 
 #Run lm surfaceGDDs:
+#LM:
 lm_gdd <- lm(massloss_mgday ~ surfaceGDDs, data = ShrubLitter_DATA)
 plot(lm_gdd) 
 summary(lm_gdd)
 tab_model(lm_gdd, show.stat=T, show.se=T, show.ci = F , digits = 3)
 
-#PLOT:==========
+#LMER:
 names(ShrubLitter_DATA)
-ShrubLitter_DATA$B <-  as.factor(ShrubLitter_DATA$burialdepth_cm)
+soilgdds_model2 <- lmer(massloss_gday ~ surfaceGDDs + (1|site), data = ShrubLitter_DATA) 
+tab_model(soilgdds_model2, show.stat=T, show.se=T, show.ci = F , digits = 7)
 
-, shape = B
 
-ggplot(aes(y = massloss_mgday, x = surfaceGDDs, shape= B), data = ShrubLitter_DATA) +
-  geom_point(size = 4,aes(fill=months), stroke = 0.5) +
-  labs( title = "",
+#STATS TABLE:=====
+library(officer)
+library(flextable)
+
+# Perform linear regression for each group (factor B) and calculate R-squared and p-value
+regression_results <- ShrubLitter_DATA %>%
+  group_by(Burial_cm,species) %>%
+  summarise(
+    R2 =round(  summary(lm(massloss_mgday ~ surfaceGDDs))$r.squared,2),
+    p_value = round( coef(summary(lm(massloss_mgday ~ surfaceGDDs)))["surfaceGDDs", "Pr(>|t|)"],5))
+
+doc <- read_docx() # Create a new Word document
+ft <- flextable(regression_results)# Convert regression_results to a flextable
+doc <- body_add_flextable(doc, value = ft) # Insert the flextable into the Word document
+print(doc, target = "ShrubLitter_regression_results.docx")# Save the Word document:
+
+
+#THE PLOT:==========
+#Different lines in lm:
+ggplot(aes(y = massloss_mgday, x = surfaceGDDs, shape = Burial_cm, fill = months), data = ShrubLitter_DATA) +
+  geom_point(size = 4, aes(fill = factor(months)), stroke = 0.5) +
+  labs(
+    title = "",
     y = "Mass loss (mg/day)",
     x = "Growing degree days",
-    shape = "Deployment depth: ",
-    color = "Months of deployment:") +       #,  shape = "Site" or burialdepth_cm
-  
-  scale_shape_manual(values = c(19,15))+
-  scale_color_manual(values = c("white", "grey70", "#666666")) +
-  #scale_fill_manual(values = c("white", "grey80", "#666666")) +
-  facet_grid(. ~ species) +
-  theme_bw() +
-    guides(colour = guide_legend(show = FALSE)) +
-
-    geom_smooth( method = "lm", se = TRUE, size = 1, color = "black") +
-  theme(legend.position = "top",
-    legend.key.size = unit(1.5, "lines"),
-    legend.background = element_rect(fill = "white", color = "black"),
-    axis.title = element_text(size = 18),
-    panel.grid = element_blank(),
-    strip.text = element_text(face = "italic", size = 14),
-    axis.text = element_text(size = 12, color = "black"))
-
-
-#ggsave(width = 12, height = 7, file = "MassLoss_Species_Lm.png")
-
-
-#No split by Burial Depth Plot:
-ggplot(aes(y = massloss_mgday, x = surfaceGDDs, color = months), data = ShrubLitter_DATA) +
-  geom_point(size = 4, aes(fill = months), shape = 21, stroke = 0.5, color = "black") +
-  labs( title = "",
-    y = "Mass Loss (mg/day)",
-    x = "Growing Degree Days",
-    fill = "Months of Deployment:") +       #,  shape = "Site" or burialdepth_cm
-  
-  scale_color_manual(values = c("white", "grey80", "#666666")) +
+    shape = "Deployment depth (cm): ",
+    fill = "Months of deployment:"
+  ) +
+  scale_shape_manual(values = c(21, 22)) +
   scale_fill_manual(values = c("white", "grey80", "#666666")) +
   facet_grid(. ~ species) +
   theme_bw() +
-    guides(colour = guide_legend(show = FALSE)) +
+  guides(colour = guide_legend(show = FALSE)) +
 
-    geom_smooth( method = "lm", se = TRUE, size = 1, color = "red") +
-  theme(legend.position = "top",
+  geom_smooth(method = "lm",
+    se = TRUE,
+    size = 1,
+    color = "black",  # Set the color to black
+    aes(fill = NA, linetype = Burial_cm)) +  # Use factor(Burial_cm) to separate lines by Burial_cm-variable
+    
+  scale_linetype_manual(values = c("solid", "dashed")) +  # Set linetype for B levels
+  
+    theme(legend.position = "top",
     legend.key.size = unit(1.5, "lines"),
     legend.background = element_rect(fill = "white", color = "black"),
     axis.title = element_text(size = 18),
     panel.grid = element_blank(),
     strip.text = element_text(face = "italic", size = 14),
-    axis.text = element_text(size = 12, color = "black"))
+    axis.text = element_text(size = 12, color = "black"))+
+  
+    guides(linetype = guide_legend(show = FALSE)) 
 
-#ggsave(width = 12, height = 7, file = "MassLoss_Species_Lm.png")
+#ggsave(width = 12, height = 7, file = "GDD_MassLoss_BySpecies_ByBurialDepths_2LineTypes.png")
